e7b0209722894e0c6657ec4e5f4ff99a1fbec996
larrythanks <larry.thanks@163.com>
2022-04-27 09:37:04 +0800

    jia x

--- main.c
+++ main.c
@@ -3,10 +3,17 @@
 #include "String.h"
 #include "math.h"
 
-int i = 0, j = 0;
 int loop = 0;
 void main(void)
 {
+    for (size_t i = 0; i < 10; i++)
+    {
+        for (size_t j = 0; j < 10; j++)
+        {
+            printf("%DX%d=%d", i, j, i * j);
+        }
+    }
+
     printf("this is test%d\r\n", loop);
     printf("j++ is%d\r\n", j++);
     printf("++i is%d\r\n", ++i);
